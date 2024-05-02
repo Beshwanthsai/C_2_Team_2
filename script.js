05540ada93b1949e20a30cc1f0533b9a1eff051f
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2024-05-01 22:19:32 -0700

    created a basic page on shopping website named laptop shopzz

---
+++ script.js
@@ -0,0 +1,8 @@
+function addToCart() {
+    // Simulate adding to cart (replace with your cart logic)
+    alert("Item added to cart!");
+  
+    // Redirect to cart page
+    window.location.href = "cart.html";
+  }
+  
