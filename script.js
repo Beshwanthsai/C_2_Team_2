261d01cca1bc09a167526ff1c9816a58736ec689
Rishitha <rishithakolli14>
2024-06-26 12:18:56 +0530

    modified about,cart,home,products,login pages and deleted support and script.js pages.

--- script.js
+++
@@ -1,8 +0,0 @@
-function addToCart() {
-    // Simulate adding to cart (replace with your cart logic)
-    alert("Item added to cart!");
-  
-    // Redirect to cart page
-    window.location.href = "cart.html";
-  }
-  
